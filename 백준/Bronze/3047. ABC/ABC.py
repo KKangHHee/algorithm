36418ffb42112eeea906c13166f374a079a855c4
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2025-02-04 11:50:07 +0900

    [Bronze III] Title: ABC, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3047. ABC/ABC.py
+++ 백준/Bronze/3047. ABC/ABC.py
@@ -1,3 +1,3 @@
 num1, num2, num3 = sorted(map(int, input().split())) ## sorted :: 정렬
 order = input().strip().upper()
-print(order.replace('A', str(num1)+" ").replace('B', str(num2)+" ").replace('C', str(num3)+" "))
+print(*[str({'A': num1, 'B': num2, 'C': num3}[x]) for x in order])
