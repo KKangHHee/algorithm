9209e81730c6aa5933b9a06bacb1aaa38082f96e
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2025-03-10 23:26:30 +0900

    [Silver V] Title: 삼각형만들기, Time: 688 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
@@ -1,13 +1,18 @@
-def resultN(N):
-    if N < 3:
-        return 0
-    count = 0
-    for a in range(1, N // 3 + 1):
-        for b in range(a, (N - a) // 2 + 1):
-            c = N - a - b
-            if a + b > c:
-                count += 1
-    return count
-
 N = int(input())
-print(resultN(N))
+count = 0
+
+if N < 3:
+    print(0)
+else:
+    for i in range(1, N+1):
+        for j in range(i, N+1):
+            sum_ij = i + j
+            k = N - sum_ij
+        
+            if k < sum_ij: # 삼각형 빗변이 양변의 합보다 작을 때 삼각형 성립
+                if k < j:
+                    break
+                count += 1
+            else:
+                continue
+    print(count)
