470dbf78199dc417f9756c71a8494f76a9a93d91
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2026-01-09 15:26:37 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,9 +1,3 @@
-// 알고리즘) 64, 32, 16, 8, 4, 2, 1
-// 규칙)
-// 입력)
-// 츨력)
-
-
 import java.io.*;
 import java.util.*;
 
@@ -11,11 +5,7 @@ public class Main{
     public static void main(String[] args) throws IOException{
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
             int x = Integer.parseInt(br.readLine());
-            int count = 0;
-            while(x>0){
-                if((x&1)==1) count++;
-                x >>=1;
-            }
+						int count = Integer.bitCount(x);
             System.out.print(count);
         }
     }
